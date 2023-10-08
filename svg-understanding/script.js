9fc3d6016391a3c7f45c2b87c88a48d82abf9ea0
Rakhat Alpysbai <rakhatdev@gmail.com>
2023-10-08 21:02:36 +0600

    file formatting

--- svg-understanding/script.js
+++ svg-understanding/script.js
@@ -1,22 +1,22 @@
-console.log("SVG Understanding!")
+console.log("SVG Understanding!");
 
 // Task: draw simple bar chart from json data using svg, rect
-let buildings = []
+let buildings = [];
 
 d3.json("./data/buildings.json").then((data) => {
-  buildings = data.map((item) => ({ ...item, height: +item.height }))
-  drawChart()
-})
+  buildings = data.map((item) => ({ ...item, height: +item.height }));
+  drawChart();
+});
 
 function drawChart() {
-  console.log(buildings)
+  const y = d3.scaleLinear().domain([0, 828]).range([0, 400]);
 
   const svg = d3
     .select("#app")
     .append("svg")
     .attr("width", 600)
     .attr("height", 400)
-    .style("border", "1px solid #333")
+    .style("border", "1px solid #333");
 
   svg
     .selectAll("rect")
@@ -24,8 +24,7 @@ function drawChart() {
     .enter()
     .append("rect")
     .attr("width", 35)
-    .attr("height", (item) => item.height)
+    .attr("height", (item) => y(item.height))
     .attr("fill", "gray")
-    .attr("x", (_, idx) => 50 * idx)
-    .attr("y", (item) => 400 - item.height)
+    .attr("x", (_, idx) => 50 * idx);
 }
